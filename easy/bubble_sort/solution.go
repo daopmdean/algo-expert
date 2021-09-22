ac5dcb7af9375f600747a71259e8c8167a7314a0
Dao Pham <daopm.dean@gmail.com>
2021-09-22 20:05:48 +0700

    bubble sort

---
+++ easy/bubble_sort/solution.go
@@ -0,0 +1,14 @@
+package easy
+
+func BubbleSort(array []int) []int {
+	for i := 0; i < len(array)-1; i++ {
+		for j := i + 1; j < len(array); j++ {
+			if array[i] > array[j] {
+				temp := array[i]
+				array[i] = array[j]
+				array[j] = temp
+			}
+		}
+	}
+	return array
+}
